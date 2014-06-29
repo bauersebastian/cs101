287232fdd689fc0dbac5b5c3a54500c31e35bf6e
Sebastian <sebastian@pisada.de>
2014-06-29 23:26:24 +0200

    Initial commit

---
+++ lesson2/loops.py
@@ -0,0 +1,47 @@
+i = 0
+while i<=10:
+    print i
+    i = i+1
+
+z = 0
+while z!=10:
+    z = z+1
+    print z
+
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+
+def print_numbers(end_number):
+    start_number = 1
+    while start_number <= end_number:
+        print start_number
+        start_number = start_number+1
+
+print print_numbers(255)
+
+#print_numbers(3)
+#>>> 1
+#>>> 2
+#>>> 3
+
+# Define a procedure, factorial, that
+# takes one number as its input
+# and returns the factorial of
+# that number.
+
+def factorial(n):
+
+
+
+
+#print factorial(4)
+#>>> 24
+#print factorial(5)
+#>>> 120
+#print factorial(6)
+#>>> 720
